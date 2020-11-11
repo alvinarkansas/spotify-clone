5ef954732def5e7588fa29577ced943543c47a9d
Alvin Arkansas <alvinarkansas@gmail.com>
2020-11-11 18:56:45 +0700

    added recommendation with reusable component

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import ExtendedCard from './components/ExtendedCard'
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          paddingVertical: 16,
+          paddingLeft: 16,
+          paddingRight: 8,
+          flex: 1,
+        }}
+      >
+        <View
+          style={{
+            paddingTop: 32,
+            paddingBottom: 8,
+            marginBottom: 8
+          }}
+        >
+          <Text style={[styles.textWhite, styles.textTitle]}>
+            Good afternoon
+          </Text>
+        </View>
+        <View
+          style={{
+            height: 224,
+            width: "100%",
+            marginTop: -8,
+            marginLeft: -8,
+            flexWrap: "wrap",
+          }}
+        >
+          <ExtendedCard title={"Chill"} thumbnail={require("./img/spotify_album_thumbnail_1.jpg")} />
+          <ExtendedCard title={"Time Capsule"} thumbnail={require("./img/time-capsule-blue_LARGE-en.jpg")} />
+          <ExtendedCard title={"Calm"} thumbnail={require("./img/spotify_album_thumbnail_3.jpg")} />
+          <ExtendedCard title={"Paramore"} thumbnail={require("./img/spotify_album_thumbnail_4.jpg")} />
+          <ExtendedCard title={"John Mayer"} thumbnail={require("./img/spotify_album_thumbnail_5.jpg")} />
+          <ExtendedCard title={"Pamungkas"} thumbnail={require("./img/spotify_album_thumbnail_6.jpg")} />
+        </View>
+        {/* <View
+          style={{
+            paddingTop: 4,
+            paddingBottom: 4,
+            marginBottom: 8
+          }}
+        >
+          <Text style={[styles.textWhite, styles.textTitle]}>
+            Recently Played
+          </Text>
+        </View> */}
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#0a0a0a",
+  },
+  textWhite: {
+    color: "#fff",
+  },
+  textTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
   },
 });
---
+++ components/ExtendedCard.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Text, View, Image } from "react-native";
+
+export default function ExtendedCard({ thumbnail, title }) {
+  return (
+    <View
+      style={{
+        width: "50%",
+        paddingTop: 8,
+        paddingLeft: 8,
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "rgba(255, 255, 255, .075)",
+          height: 56,
+          width: "100%",
+          borderRadius: 8,
+          flexWrap: "wrap",
+          justifyContent: "center",
+        }}
+      >
+        <Image
+          source={thumbnail}
+          style={{
+            height: "100%",
+            maxWidth: 56,
+            borderTopLeftRadius: 8,
+            borderBottomLeftRadius: 8,
+          }}
+        />
+        <Text
+          style={{
+            marginLeft: 8,
+            fontWeight: "bold",
+            fontSize: 12,
+            color: "#fff",
+          }}
+        >
+          { title }
+        </Text>
+      </View>
+    </View>
+  );
+}
